7dcb8570d336c069079582fae0c67ad3f6377d05
priyankasam86 <priyanka_dalvi86@yahoo.co.in>
2019-10-11 12:40:04 +0530

    commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//star pyramid using nested for loop - task 1
+
+for(let star1=1; star1<=5; star1++){
+
+    for(let star2=1; star2<=star1; star2++){
+
+        document.write("*");
+        
+    }
+    document.write("</br>");
+}
+
+// star pyramid using nested for loop - task 2
+
+for(let star1=1; star1<=4; star1++){
+
+    for(let star2=1; star2<=4; star2++){
+
+        document.write("*");
+        
+    }
+    document.write("</br>");
+}
